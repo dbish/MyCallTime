6c3f7293f6bbe2d74e34a63c91ab1e33db0b73c0
dbish <diamondbishop@gmail.com>
2014-12-07 19:37:44 -0800

    added form for creating a new call sheet. Controls in place, not hooked up to submission or saving yet.

--- MyCallTime/MyCallTime/__init__.py
+++ MyCallTime/MyCallTime/__init__.py
@@ -3,6 +3,8 @@ The flask application package.
 """
 
 from flask import Flask
+from MyCallTime.forms import ContactForm
+
 app = Flask(__name__)
 
 import MyCallTime.views
--- MyCallTime/MyCallTime/views.py
+++ MyCallTime/MyCallTime/views.py
@@ -5,15 +5,18 @@ Routes and views for the flask application.
 from datetime import datetime
 from flask import render_template
 from MyCallTime import app
+from MyCallTime.forms import ContactForm
 
 @app.route('/')
 @app.route('/home')
 def home():
     """Renders the home page."""
+    
     return render_template(
         'index.html',
         title='Home Page',
-        year=datetime.now().year,
+        year=datetime.now().year
+        
     )
 
 @app.route('/contact') 
@@ -34,4 +37,16 @@ def about():
         title='About',
         year=datetime.now().year,
         message='Your application description page.'
+    )
+
+@app.route('/newSheet')
+def newSheet():
+    """Renders the about page."""
+    
+    return render_template(
+        'newSheet.html',
+        title='New Sheet',
+        year=datetime.now().year,
+        message='Your application description page.',
+       
     )
