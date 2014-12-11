95dfa7144dfd203f319fb19a94b4b638906dec52
dbish <diamondbishop@gmail.com>
2014-12-10 19:24:12 -0800

    beginning to move from the html mockup to form creation through jquery. newSheet function created and talent panel being handled partially with a few lines of code. Need to turn this into a reusable function for each input area.

---
+++ MyCallTime/MyCallTime/static/scripts/loadSheet.js
@@ -0,0 +1,6 @@
+﻿$(document).ready(function () {
+    console.log('ready!');
+    talentPanel = $('#collapseTalent > .panel-body > .row > .col-lg-6');
+    talentPanel.append('<div class="input-group"><label>Talent</label></div>');
+    talentPanel.append('<div class="input-group"><div class="input-group-btn"><input type="time" class="form-control"></div><input type="text" class="form-control"></div>');
+});
