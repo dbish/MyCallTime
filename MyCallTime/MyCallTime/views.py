f8684fe5fb02337647af3de642cf13d75a1481b7
dbish <diamondbishop@gmail.com>
2015-02-12 21:48:01 -0800

    printing almost all roles to pdf now, still need to add shoot metadata and catering

--- MyCallTime/MyCallTime/views.py
+++ MyCallTime/MyCallTime/views.py
@@ -13,7 +13,7 @@ from wtforms.ext.sqlalchemy.orm import model_form
 from flask_wtf import Form
 from copy import deepcopy
 from fpdf import FPDF, HTMLMixin
-Title = "something"
+Title = " "
 
 @app.route('/')
 @app.route('/home')
@@ -164,6 +164,7 @@ from reportlab.lib.styles import getSampleStyleSheet
 from reportlab.rl_config import defaultPageSize
 from reportlab.lib.units import inch
 from io import BytesIO
+#from flask import Response
 from flask import make_response
 
 def myFirstPage(canvas, doc):
@@ -175,7 +176,9 @@ def myFirstPage(canvas, doc):
     canvas.drawCentredString(PAGE_WIDTH/2.0, PAGE_HEIGHT-108, Title)
     canvas.restoreState()
 
+
 def createPdf(title, date, location, id, shoot):
+    #buffer = StringIO()
     buffer = BytesIO()
     styleSheet = getSampleStyleSheet()
 
@@ -187,23 +190,83 @@ def createPdf(title, date, location, id, shoot):
     doc = SimpleDocTemplate(buffer, pagesize=letter)
     # container for the 'Flowable' objects
     elements = []
+    
+    talentData= [[Paragraph('<b>Talent</b>', styleSheet["BodyText"]), Paragraph('<b>Agent</b>', styleSheet["BodyText"]), Paragraph('<b>Contact</b>', styleSheet["BodyText"]), Paragraph('<b>CallTime</b>', styleSheet["BodyText"])]]
+    for talent in shoot.talent:
+        talentData.append([talent.full_name, talent.agent_name, talent.agent_phone, talent.start_time])
+    talentData.append(['', '', '', ''])
  
     data= [[Paragraph('<b>Team</b>', styleSheet["BodyText"]), Paragraph('<b>Name</b>', styleSheet["BodyText"]), Paragraph('<b>Contact</b>', styleSheet["BodyText"]), Paragraph('<b>CallTime</b>', styleSheet["BodyText"])]]
-    data.append(['Photographer', shoot.photo.photographer, "contact agent", shoot.photo.start_time])
-    data.append(['Photo Agent', shoot.photo.agent_name,  shoot.photo.agent_phone, "as needed"])
-    data.append(['Digital Tech', shoot.photo.digital_tech, shoot.photo.digitech_phone, shoot.photo.start_time])
+    
+    #-production-
+    #production company
+    data.append(['Production Company', shoot.production.company, "", ""])
+    #producer
+    data.append(['Producer', shoot.production.producer_name, shoot.production.producer_phone, shoot.production.start_time])
+    #production assist(s)
+    for assistant in shoot.production.assistants:
+        data.append(['Production Assist', assistant.assistantName, assistant.phone, shoot.production.start_time])
 
+
+    
+    #-photography-
+    #photographer photographer contact-agent start_time
+    data.append(['Photographer', shoot.photo.photographer, "contact agent", shoot.photo.start_time])
+    #photo-agent agent_name agent_phone as-needed
+    data.append(['Photo Agent', shoot.photo.agent_name,  shoot.photo.agent_phone, "as needed"])
+    #digital tech
+    data.append(['Digital Tech', shoot.photo.digital_tech, shoot.photo.digitech_phone, shoot.photo.start_time])
+    #photo 1st assist
+    data.append(['Photo 1st Assist', shoot.photo.first_ast_name, shoot.photo.first_ast_phone, shoot.photo.start_time])
+    #photo assist(s)
+    for assistant in shoot.photo.assistants:
+        data.append(['Photo Assist', assistant.assistantName, assistant.phone, shoot.photo.start_time])
+
+    #-art-
+    data.append(['Artist', shoot.art.artist, "contact agent", shoot.art.start_time])
+    data.append(['Art Agent', shoot.art.agent_name,  shoot.art.agent_phone, "as needed"])
+    data.append(['Art 1st Assist', shoot.art.first_ast_name, shoot.art.first_ast_phone, shoot.art.start_time])
+    for assistant in shoot.art.assistants:
+        data.append(['Art Assist', assistant.assistantName, assistant.phone, shoot.art.start_time])
+
+    #-makeup-
+    data.append(['Makeup Artist', shoot.makeup.artist, "contact agent", shoot.makeup.start_time])
+    data.append(['Makeup Agent', shoot.makeup.agent_name,  shoot.makeup.agent_phone, "as needed"])
+    data.append(['Makeup 1st Assist', shoot.makeup.first_ast_name, shoot.makeup.first_ast_phone, shoot.makeup.start_time])
+    for assistant in shoot.makeup.assistants:
+        data.append(['Makeup Assist', assistant.assistantName, assistant.phone, shoot.makeup.start_time])
+
+    #-hair-
+    data.append(['Hair Stylist', shoot.hair.stylist, "contact agent", shoot.hair.start_time])
+    data.append(['Hair Agent', shoot.hair.agent_name,  shoot.hair.agent_phone, "as needed"])
+    data.append(['Hair 1st Assist', shoot.hair.first_ast_name, shoot.hair.first_ast_phone, shoot.hair.start_time])
+    for assistant in shoot.hair.assistants:
+        data.append(['Hair Assist', assistant.assistantName, assistant.phone, shoot.hair.start_time])
+
+    #-wardrobe-
+    data.append(['Wardrobe Stylist', shoot.wardrobe.stylist, "contact agent", shoot.wardrobe.start_time])
+    data.append(['Wardrobe Agent', shoot.wardrobe.agent_name,  shoot.wardrobe.agent_phone, "as needed"])
+    data.append(['Wardrobe 1st Assist', shoot.wardrobe.first_ast_name, shoot.wardrobe.first_ast_phone, shoot.wardrobe.start_time])
+    for assistant in shoot.wardrobe.assistants:
+        data.append(['Wardrobe Assist', assistant.assistantName, assistant.phone, shoot.wardrobe.start_time])
+
+    #don't print rows where there is no name for a task
+    data = [row for row in data if row[1] != '']
+    
+
+    talentTable = Table(talentData)
     t=Table(data)
     
+    elements.append(talentTable)
     elements.append(t)
-
     # write the document to buffer
+    
     doc.build(elements, onFirstPage=myFirstPage)   
     pdf = buffer.getvalue()
     buffer.close()
     return pdf
 
-@app.route('/pdf/<int:shoot_id>', methods=['GET'])
+@app.route('/viewpdf/<int:shoot_id>', methods=['GET'])
 def createPDF(shoot_id):
     if 'email' not in session:
         return redirect(url_for('signin'))
@@ -215,4 +278,17 @@ def createPDF(shoot_id):
 
     response.headers['Content-Type']='application/pdf'
     response.headers['Content-Disposition'] = 'filename="shoot.pdf"'
+    return response
+
+@app.route('/savepdf/<int:shoot_id>', methods=['GET'])
+def savePDF(shoot_id):
+    if 'email' not in session:
+        return redirect(url_for('signin'))
+
+    shoot = db.session.query(Shoots).get(shoot_id)
+    pdf = createPdf(shoot.name, shoot.date, shoot.location, shoot.ID, shoot)
+    response = make_response(pdf)
+    response.headers['Content-Type']='application/pdf'
+    response.headers['Content-Disposition'] = 'attachment; filename="shoot.pdf"'
+
     return response
