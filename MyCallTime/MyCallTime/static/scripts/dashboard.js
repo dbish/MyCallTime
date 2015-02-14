9c02464c3e2089e281a41f08487c0c0969861d9e
dbish <diamondbishop@gmail.com>
2015-02-14 14:37:17 -0800

    dashboard updated to control edittin, duplicating, and deleting through radio button choice

---
+++ MyCallTime/MyCallTime/static/scripts/dashboard.js
@@ -0,0 +1,31 @@
+﻿$(document).ready(function () {
+    var count = $("input[type='radio']").length
+    if (count > 0){
+        $("input[type=radio]:first-child").attr('checked', 'checked');
+    };
+
+    $('#editShoot').click(function()
+    {
+
+        var shootID = $("input[type='radio']:checked").val();
+        window.location = "/shoots/"+shootID;
+
+        }
+    );
+
+    $('#duplicateShoot').click(function () {
+        var shootID = $("input[type='radio']:checked").val();
+        window.location = "/copy/" + shootID;
+
+    }
+       );
+  
+    $('#deleteShoot').click(function () {
+        var shootID = $("input[type='radio']:checked").val();
+        window.location = "/delete/" + shootID;
+
+    }
+     );
+
+  
+});
