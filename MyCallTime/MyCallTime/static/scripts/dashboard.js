64c16bdfe6969922e80241d65f92f2146b54b55e
dbish <diamondbishop@gmail.com>
2015-02-24 19:11:01 -0800

    added search to shoots table

--- MyCallTime/MyCallTime/static/scripts/dashboard.js
+++ MyCallTime/MyCallTime/static/scripts/dashboard.js
@@ -27,5 +27,15 @@
     }
      );
 
+    $('#filter').keyup(function () {
+
+        var rex = new RegExp($(this).val(), 'i');
+        $('.searchable tr').hide();
+        $('.searchable tr').filter(function () {
+            return rex.test($(this).text());
+        }).show();
+
+    });
+
   
 });
